7d010e817f3496f377fa94e79acbb7ca7b01df85
fedjensa <jens.hedberg@medieinstitutet.se>
2017-10-09 00:42:24 +0200

    project start

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="wp-content/themes/complete_wp_dev/style.css">
+    <title>Document</title>
+</head>
+<body>
+
+    <header>Header Content</header>
+
+    <aside>Left sidebar</aside>
+
+    <section>
+        <div class="section group">
+            <div class="col span_1_of_3">
+            This is column 1
+            </div>
+            <div class="col span_1_of_3">
+            This is column 2
+            </div>
+            <div class="col span_1_of_3">
+            This is column 3
+            </div>
+        </div>
+    </section>
+
+    <aside>Right Sdiebar</aside>
+
+    <footer>Footer Content</footer>
+    
+</body>
+</html>
